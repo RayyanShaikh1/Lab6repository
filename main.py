b7b0513e542c46da637eedd7065ec24b8205c4bc
RayyanShaikh1 <146393855+RayyanShaikh1@users.noreply.github.com>
2023-10-30 21:21:15 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# encoder function adds 3 to each digit in the password
+def encode(password):
+    if len(password) != 8 or not password.isdigit():
+        return False
+    else:
+        encoded_pas = ''
+        for digit in password:
+            original = int(digit)
+            encoded_dig = (original + 3) % 10
+            encoded_pas += str(encoded_dig)
+        return encoded_pas
+# decoder functions subtracts 3 from each digit in the encoded password
+
+
+new_password = ''
+# main while loop
+while True:
+    print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
+    option = int(input('Please enter an option: '))
+    if option == 1:
+        password = input('Please enter your password to encode: ')
+        try:
+            new_password += encode(password)
+            print('Your password has been encoded and stored!')
+        except:
+            print("error message")
